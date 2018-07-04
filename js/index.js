d405ed1691dced75c2d201db54d12f20eeda3585
Daria Bilous <daria.bilous@icloud.com>
2018-07-04 18:33:38 +0300

    remove js from html; create js file instead

---
+++ js/index.js
@@ -0,0 +1,36 @@
+
+// let a,b,result;
+//         a=0;
+//         b=1;
+//         result=b;
+//         for(let i=1; i<100; i++)
+//         {
+//             document.write(result+"<br/>");
+//             result=a+b;
+//             a=b;
+//             b=result;
+//         }
+let number = parseFloat(prompt('Enter number'));
+const result = fibiNum(number);
+alert(number + ' in the fibonacci sequence holds ' + result);
+
+function fibNum(number) {
+
+    let fibPrev=1;
+    let fibPrePrev=1;
+    let fibNext;
+    
+    if(number==1 || number==2){
+        return fibPrev;
+    } else {
+
+    for(let i=3; i <= number; i++) {
+        
+        fibNext = fibPrev + fibPrePrev;
+        fibPrePrev = fibPrev;
+        fibPrev = fibNext;
+    }
+    
+    return fibNext;
+   }
+} 
